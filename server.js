ff5fd126d3352033bc59bae26835ecf19f42956f
ABHAY <abhaychadha15@gmail.co>
2024-04-14 19:39:44 -0400

    changes

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 //const session = require("express-session");
-const {allowInsecurePrototypeAccess} =require('@handlebars/allow-prototype-access');
-const Handlebars = require('handlebars'); 
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const Handlebars = require("handlebars");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const exphbs  = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const requireToken = require("./middleware/requiredToken");
 const port = 8000;
 const app = express();
@@ -16,7 +18,7 @@ const app = express();
 //CONNECTION TO  ATLAS DB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 let db = mongoose.connection;
 
@@ -30,22 +32,28 @@ db.on("error", function (err) {
   console.log("DB Error");
 });
 
-var movies = require('./models/movies');
-var User = require('./models/user');
+var movies = require("./models/movies");
+var User = require("./models/user");
 
 // Initialize built-in middleware for urlencoding and json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('.hbs', exphbs.engine({extname:'.hbs',
-handlebars: allowInsecurePrototypeAccess(Handlebars)
-}));
-app.set('view engine','.hbs');
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    extname: ".hbs",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+app.set("view engine", ".hbs");
 
-
-app.get('/', (req, res) => {
-  res.render('home', {  title: "Welcome to the project" });
+// app.get('/', (req, res) => {
+//   res.render('home', {  title: "Welcome to the project" });
+// });
+app.get("/", (req, res) => {
+  res.json({ message: "Lets get started !!" });
 });
 //sign-up route
 app.get("/signup", async (req, res) => {
@@ -71,7 +79,7 @@ app.get("/signup", async (req, res) => {
   } catch (err) {}
 });
 
-// Sign-in route 
+// Sign-in route
 app.get("/signin", async (req, res) => {
   var { email, password } = req.body;
 
@@ -100,59 +108,74 @@ app.get("/signin", async (req, res) => {
   }
 });
 //pagination route
-app.get('/api/allMovies/:page/:perPage/:title', function(req, res) {
+app.get("/api/allMovies/:page/:perPage/:title", function (req, res) {
   const page = parseInt(req.params.page);
   const perPage = parseInt(req.params.perPage);
-  const titleFilter = req.params.title || '';
+  const titleFilter = req.params.title || "";
   let query = {};
   if (titleFilter) {
-      query = { title: { $regex: new RegExp(titleFilter, 'i') } };
+    query = { title: { $regex: new RegExp(titleFilter, "i") } };
   }
 
-  movies.countDocuments(query)
-      .then(totalCount => {
-          const totalPages = Math.ceil(totalCount / perPage);
+  movies
+    .countDocuments(query)
+    .then((totalCount) => {
+      const totalPages = Math.ceil(totalCount / perPage);
 
-          if (page < 1 || page > totalPages) {
-              throw new Error('Invalid page number');
-          }
+      if (page < 1 || page > totalPages) {
+        throw new Error("Invalid page number");
+      }
 
-          const skip = (page - 1) * perPage;
+      const skip = (page - 1) * perPage;
 
-          return movies.find(query).skip(skip).limit(perPage);
-      })
-      .then(movieList => {
-        res.render('paginationmovies', {  movie: movieList }); 
-      })
-      .catch(err => {
-          res.status(500).send(err.message);
-      });
+      return movies.find(query).skip(skip).limit(perPage);
+    })
+    .then((movieList) => {
+      res.render("paginationmovies", { movie: movieList });
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
 });
-
-
 
 //GET route to fetch all movies
-app.get('/allMovies', (req, res) => {
-  movies.find()
-      .then(movie => {
-          if (movie && movie.length > 0) {
-           res.render('allMovies', {  movie: movie });  
-            
-          } else {
-              res.send('No movies available');
-          }
-      })
-      .catch(err => {
-          res.status(500).send(err.message);
-      });
+// app.get("/allMovies", (req, res) => {
+//   movies
+//     .find()
+//     .then((movie) => {
+//       if (movie && movie.length > 0) {
+//         res.render("allMovies", { movie: movie });
+//       } else {
+//         res.send("No movies available");
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).send(err.message);
+//     });
+// });
+// --------
+app.get("/allMovies", (req, res) => {
+  movies
+    .find()
+    .then((movie) => {
+      if (movie && movie.length > 0) {
+        res.json(movie); // Return movie data as JSON
+      } else {
+        res.status(404).json({ message: "No movies available" }); // Return a JSON message if no movies are available
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message }); // Return a JSON error message with status code 500
+    });
 });
+// -------
 
 
 
 //CREATE MOVIES
 // POST route to create a new movie
 // app.post('/api/Movies', (req, res) => {
-//   const data = req.body; 
+//   const data = req.body;
 
 //   const newMovie = new movies(data);
 
@@ -165,77 +188,80 @@ app.get('/allMovies', (req, res) => {
 //       });
 // });
 
-app.post('/api/Movies', requireToken, (req, res) => {
-  const data = req.body; 
+app.post("/api/Movies", requireToken, (req, res) => {
+  const data = req.body;
 
   const user = req.user;
   const newMovie = new movies(data);
 
-  newMovie.save()
-      .then(savedMovie => {
-          res.status(201).json(savedMovie);
-      })
-      .catch(err => {
-          res.status(500).json({ error: err.message });
-      });
+  newMovie
+    .save()
+    .then((savedMovie) => {
+      res.status(201).json(savedMovie);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
-
 
 //GET MOVIES BY ID
 // GET route to fetch a specific movie by its _id
-app.get('/api/movies/:id', (req, res) => {
+app.get("/api/movies/:id", (req, res) => {
   const movieId = req.params.id;
 
-  movies.findById(movieId)
-      .then(movie => {
-          if (!movie) {
-              return res.status(404).json({ error: 'Movie not found' });
-          }
-          res.json(movie);
-      })
-      .catch(err => {
-          res.status(500).json({ error: err.message });
-      });
+  movies
+    .findById(movieId)
+    .then((movie) => {
+      if (!movie) {
+        return res.status(404).json({ error: "Movie not found" });
+      }
+      res.json(movie);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
-
 
 //UPDATE MOVIES
 // PUT route to update a specific movie by its _id
-app.put('/api/movies/:id', (req, res) => {
+app.put("/api/movies/:id", (req, res) => {
   const movieId = req.params.id;
   const updateData = req.body;
 
-  movies.findByIdAndUpdate(movieId, updateData, { new: true })
-      .then(updatedMovie => {
-          if (!updatedMovie) {
-              return res.status(404).json({ error: 'Movie not found or could not be updated' });
-          }
-          res.json(updatedMovie);
-      })
-      .catch(err => {
-          res.status(500).json({ error: err.message });
-      });
+  movies
+    .findByIdAndUpdate(movieId, updateData, { new: true })
+    .then((updatedMovie) => {
+      if (!updatedMovie) {
+        return res
+          .status(404)
+          .json({ error: "Movie not found or could not be updated" });
+      }
+      res.json(updatedMovie);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
 
- 
 //DELETE MOVIES
 // DELETE route to delete a specific movie by its _id
-app.delete('/api/movies/:id', (req, res) => {
+app.delete("/api/movies/:id", (req, res) => {
   const movieId = req.params.id;
 
-  movies.findByIdAndDelete(movieId)
-      .then(deletedMovie => {
-          if (!deletedMovie) {
-              return res.status(404).json({ error: 'Movie not found or could not be deleted' });
-          }
-          res.json({ message: 'Movie deleted successfully' });
-      })
-      .catch(err => {
-          res.status(500).json({ error: err.message });
-      });
+  movies
+    .findByIdAndDelete(movieId)
+    .then((deletedMovie) => {
+      if (!deletedMovie) {
+        return res
+          .status(404)
+          .json({ error: "Movie not found or could not be deleted" });
+      }
+      res.json({ message: "Movie deleted successfully" });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
-
-
 
 //WE WILL TRY TO ADD MORE ROUTES AFTER COMPLETING ABOVE ONES
 
