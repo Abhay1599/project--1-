0b6b6e9882992b1ea9ec20f37fb56903bf4458b1
ABHAY <abhaychadha15@gmail.co>
2024-04-16 18:25:22 -0400

    changes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,10 +1,10 @@
-/********************************************************************************* 
- * * ITE5315 – Project 
- * * I declare that this assignment is my own work in accordance with Humber Academic Policy. 
- * * No part of this assignment has been copied manually or electronically from any other source 
- * * (including web sites) or distributed to other students. 
- * * * Group member Name: ABHAY & Prarabdha Student IDs: N01581911 & N01578947 Date: 16th April 2024 
- * *********************************************************************************/
+// /********************************************************************************* 
+//  * * ITE5315 – Project 
+//  * * I declare that this assignment is my own work in accordance with Humber Academic Policy. 
+//  * * No part of this assignment has been copied manually or electronically from any other source 
+//  * * (including web sites) or distributed to other students. 
+//  * * * Group member Name: ABHAY & Prarabdha Student IDs: N01581911 & N01578947 Date: 16th April 2024 
+//  * *********************************************************************************/
 const express = require("express");
 const mongoose = require("mongoose");
 const requireAuth = require("../middleware/requiredToken");
